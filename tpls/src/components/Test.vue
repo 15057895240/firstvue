104048623a959c972f665882cc936c0aada00b43
leo.sho <443003203@qq.com>
2018-01-16 11:27:13 +0800

    add test.vue

---
+++ tpls/src/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Vue is Awesom</h1>
+    <h2>{{msg}}</h2>
+  </div>
+</template>
+<script>
+export default {
+  data:()=>{
+    return {msg:'it is a test'}
+  }
+}
+</script>
